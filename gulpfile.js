28d3987c6f23fd600dc5f0f5248fb66571c182ec
Usman Khwaja <u.khwaja@gmail.com>
2018-07-22 00:38:14 +0500

    push and see

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+let gulp = require('gulp'),
+  plumber = require('gulp-plumber'),
+  scss = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  prefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify-es').default,
+  notify = require('gulp-notify'),
+  sourcemaps = require('gulp-sourcemaps'),
+  clean = require('gulp-clean'),
+  sequence = require('gulp-sequence'),
+  environments = require('gulp-environments'),
+  development = environments.development,
+  production = environments.production,
+  browserSync = require('browser-sync').create();
+  
+var destination = production() ? './dist/' : './temp/';
+
+gulp.task('scss', () => {
+  gulp.src('./src/assets/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(concat('styles.min.css'))
+    .pipe(development(sourcemaps.init()))
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
+    .on('error', function (err) {
+      notify().write(err);
+      this.emit('end');
+    })
+    .pipe(prefixer({
+      cascade: false
+    }))
+    .pipe(development(sourcemaps.write('.')))
+    .pipe(gulp.dest(destination + 'assets/css/'))
+    .pipe(development(browserSync.stream()));
+  // .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('js', () => {
+  gulp.src('./src/assets/js/main.js')
+    .pipe(plumber())
+    .pipe(concat('main.min.js'))
+    .pipe(development(sourcemaps.init()))
+    .pipe(production(uglify()))
+    .pipe(development(sourcemaps.write('.')))
+    .pipe(gulp.dest(destination + 'assets/js/'));
+  development() ? browserSync.reload() : '';
+});
+
+gulp.task('index', () => {
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest(destination));
+  development() ? browserSync.reload() : '';
+});
+
+gulp.task('clean', () => {
+  return gulp.src(['./dist', './temp'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copy', () => {
+  gulp.src(['./src/assets/img/*', './src/assets/pdf/*'], {base : 'src'})
+    .pipe(gulp.dest(destination));
+});
+
+gulp.task('watch', ['build'], () => {
+  browserSync.init({
+    server: './temp',
+    browser: 'C:/Program Files/Opera/launcher.exe'
+    // browser: 'C:/Program Files/Mozilla Firefox/firefox.exe'
+    // browser: 'microsoft-edge:http://localhost:3000'
+  });
+
+  gulp.watch('./src/assets/scss/styles.scss', ['scss']);
+  gulp.watch('./src/assets/js/main.js', ['js']);
+  gulp.watch('./src/index.html', ['index']);
+});
+
+gulp.task('build', sequence('clean', ['copy', 'js', 'scss', 'index']));
---
+++ src/assets/js/main.js
@@ -0,0 +1,171 @@
+let isToggle = false;
+
+const toggleMenu = () => {
+  const slideHideElms = document.querySelectorAll('.slide-hide');
+  if (!isToggle) {
+    document.querySelector('.menu-button').classList.add('open');
+    document.querySelector('.menu-overlay').style.transform = 'translateY(0)';
+    document.querySelector('.menu-overlay').style.zIndex = '6';
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('footer').style.display = 'none';
+    document.getElementById('stuff').style.display = 'none';
+    [].forEach.call(slideHideElms, (elm) => {
+      elm.style.transform = 'translateY(-3rem)';
+      elm.style.opacity = '0';
+      elm.style.visibility = 'hidden';
+    });
+    isToggle = true;
+  } else {
+    document.querySelector('.menu-button').classList.remove('open');
+    document.querySelector('.menu-overlay').style.transform = 'translateY(-100vh)';
+    document.querySelector('.menu-overlay').style.zIndex = '-1';
+    document.getElementById('main').style.display = 'block';
+    document.getElementById('footer').style.display = 'block';
+    document.getElementById('stuff').style.display = 'block';
+    [].forEach.call(slideHideElms, (elm) => {
+      elm.style.transform = 'translateY(0)';
+      elm.style.opacity = '1';
+      elm.style.visibility = 'visible';
+    });
+    isToggle = false;
+  }
+};
+
+const rateMe = () => {
+  let int = 0;
+  const lists = document.querySelectorAll('.skill');
+  [].forEach.call(lists, (el) => {
+    const rate = parseInt(el.getAttribute('rating'), 10);
+    const span = document.createElement('span');
+    el.appendChild(span);
+    span.classList.add('rating');
+    int = 0;
+    while (int < rate) {
+      const star = document.createElement('i');
+      star.classList.add('fa');
+      star.classList.add('fa-star');
+      star.setAttribute('aria-hidden', 'true');
+      span.appendChild(star);
+      int += 1;
+    }
+    int = 0;
+    while (int < 5 - rate) {
+      const star = document.createElement('i');
+      star.classList.add('far');
+      star.classList.add('fa-star');
+      star.setAttribute('aria-hidden', 'true');
+      span.appendChild(star);
+      int += 1;
+    }
+  });
+};
+
+const revealAll = () => {
+  const whatsapp = document.getElementById('whatsapp');
+  const email = document.getElementById('email');
+  const skype = document.getElementById('skype');
+  whatsapp.innerHTML = '5162700';
+  whatsapp.parentElement.setAttribute('href', 'whatsapp://send?text=Hello World!&phone=923215162700');
+  whatsapp.parentElement.style.cursor = 'pointer';
+  email.innerHTML = 'numbfall';
+  email.parentElement.setAttribute('href', 'mailto:numbfall@live.com');
+  email.parentElement.style.cursor = 'pointer';
+  skype.innerHTML = 'numbfall';
+  skype.parentElement.setAttribute('href', 'skype:numbfall?userinfo');
+  skype.parentElement.style.cursor = 'pointer';
+  document.querySelector('.caution').style.display = 'inline-block';
+};
+
+const showRecaptcha = () => {
+  if (document.getElementById('g-recaptcha').style.display !== 'flex') {
+    document.getElementById('g-recaptcha').style.display = 'flex';
+    document.querySelector('.info a').classList.remove('shake');
+  } else {
+    document.getElementById('g-recaptcha').style.display = 'none';
+  }
+};
+
+const scrollToMiddle = () => {
+  const element = document.getElementById('contacts');
+  const elementRect = element.getBoundingClientRect();
+  const absoluteElementTop = elementRect.top + window.pageYOffset + 200;
+  const middle = absoluteElementTop - window.innerHeight / 2;
+  window.scroll({
+    top: middle,
+    left: 0,
+    behavior: 'smooth'
+  });
+};
+
+const verifyCallback = (token) => {
+  setTimeout(() => {
+    if (document.getElementById('g-recaptcha')) {
+      document.getElementById('g-recaptcha').remove();
+    }
+  }, 600);
+  scrollToMiddle();
+  const info = document.querySelector('.info a');
+  info.classList.add('disabled');
+  info.removeAttribute('href');
+  info.innerHTML = '<i class="fa fa-fw fa-check-circle fa-lg"></i>&nbsp;Contacts unredacted&nbsp;';
+  revealAll();
+};
+
+var onloadCallback = () => grecaptcha.render('g-recaptcha', {
+  'sitekey': '6LfUyRwUAAAAACXG2B6PtvpeBRp8qlRFVB7wrd_A',
+  'callback': verifyCallback
+});
+
+const touchTips = () => {
+  const abbrs = document.querySelectorAll('main abbr');
+  [].forEach.call(abbrs, (abbr) => abbr.addEventListener('touchstart', () => {
+    abbr.classList.add('touched');
+    setTimeout(() => {
+      abbr.classList.remove('touched');
+    }, 2000);
+  }));
+};
+
+const calcAge = () => {
+  const parsed = Date.parse(Date()) - Date.parse('April 22, 1988');
+  const minutes = 1000 * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+  const years = days * 365;
+  const end = Math.round(parsed / years);
+  document.getElementById('age').innerHTML = end;
+};
+
+const addTouch = () => document.documentElement.classList.remove('no-touch');
+
+const onloadFonts = () => WebFont.load({
+  google: {
+    families: ['Inconsolata:400,700', 'Vollkorn:400,400i,700,700i', 'Vollkorn+SC:400,700']
+  }
+});
+
+const scrollDown = () => document.getElementsByTagName('main')[0].scrollIntoView({
+  behavior: 'smooth',
+  block: 'start'
+});
+
+const scrollUp = () => window.scroll({
+  top: 0,
+  left: 0,
+  behavior: 'smooth'
+});
+
+const ready = () => {
+  touchTips();
+  calcAge();
+  rateMe();
+};
+
+document.addEventListener('DOMContentLoaded', ready, {
+  capture: false
+});
+document.addEventListener('touchstart', addTouch, {
+  capture: false,
+  passive: true,
+  once: true
+});
