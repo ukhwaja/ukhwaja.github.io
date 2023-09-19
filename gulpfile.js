889720f62324d40efed83b970639627fd27cf4e9
Usman Khwaja <u.khwaja@gmail.com>
2023-09-18 23:59:37 -0400

    migrate to gulp 4 and scope minisite filetype

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-let gulp = require('gulp'),
-  minisite = require('gulp-minisite'),
-  plumber = require('gulp-plumber'),
-  scss = require('gulp-sass')(require('sass')),
-  concat = require('gulp-concat'),
-  prefixer = require('gulp-autoprefixer'),
-  uglify = require('gulp-uglify-es').default,
-  notify = require('gulp-notify'),
-  sourcemaps = require('gulp-sourcemaps'),
-  clean = require('gulp-clean'),
-  sequence = require('gulp-sequence'),
-  environments = require('gulp-environments'),
-  development = environments.development,
-  production = environments.production,
-  browserSync = require('browser-sync').create();
+const {watch, series, src, dest, parallel } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const environments = require('gulp-environments');
+const minisite = require('gulp-minisite');
+const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
+const prefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify-es').default;
+const notify = require('gulp-notify');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+
+let development = environments.development;
+let production = environments.production;
 
 var destination = production() ? './dist/' : './temp/';
 
-gulp.task('scss', () => {
-  gulp.src('./src/assets/scss/*.scss')
+function css() {
+    return src('./src/assets/scss/*.scss')
     .pipe(plumber())
     .pipe(development(sourcemaps.init()))
     .pipe(scss({
@@ -32,58 +32,63 @@ gulp.task('scss', () => {
     }))
     .pipe(concat('styles.min.css'))
     .pipe(development(sourcemaps.write('.')))
-    .pipe(gulp.dest(destination + 'assets/css/'))
+    .pipe(dest(destination + 'assets/css/'))
     .pipe(development(browserSync.stream()));
-  // .pipe(notify({ message: 'Styles task complete' }));
-});
+  }
 
-gulp.task('js', () => {
-  gulp.src('./src/assets/js/*.js')
+  function js() {
+    return src('./src/assets/js/*.js')
     .pipe(plumber())
     .pipe(development(sourcemaps.init()))
     .pipe(production(uglify()))
     .pipe(concat('main.min.js'))
     .pipe(development(sourcemaps.write('.')))
-    .pipe(gulp.dest(destination + 'assets/js/'));
-  development() ? browserSync.reload() : '';
-});
+    .pipe(dest(destination + 'assets/js/'));
+  }
 
-gulp.task('index', () => {
-  gulp.src('./src/**/*')
+  function index() {
+    return src(['./src/**/*.yml', './src/**/*.md'])
     .pipe(minisite())
-    .pipe(gulp.dest(destination));
-  development() ? browserSync.reload() : '';
-});
+    .pipe(dest(destination));
+  }
 
-gulp.task('clean', () => {
-  gulp.src(['./dist', './temp'], {
-    read: false
-  })
-    .pipe(clean());
-});
+  function copy() {
+    return src(['./src/assets/img/*', './src/assets/pdf/*'], {base: 'src/'})
+      .pipe(dest(destination));
+  }
 
-gulp.task('copy', () => {
-  gulp.src(['./src/assets/img/*', './src/assets/pdf/*'], {
-    base: 'src/'
-  })
-    .pipe(gulp.dest(destination));
-});
+  function browsersyncServe(cb){
+    browserSync.init({
+      server: {
+        baseDir: './temp'
+      }    
+    });
+    cb();
+  }
 
-gulp.task('watch', ['build'], () => {
+  function browsersyncReload(cb){
+    browserSync.reload();
+    cb();
+  }
+  
+  function watchTask() {
+    watch('./src/assets/scss/styles.scss', css);
+    watch('./src/assets/js/main.js', series(js, browsersyncReload));
+    watch(['./template/**/*.html', './src/**/*.yml', './src/**/*.md'], series(index, browsersyncReload));
+  };
 
-  browserSync.init({
-    server: './temp',
-    // browser: 'C:/Program Files/Opera/launcher.exe'
-    // browser: 'C:/Program Files/Mozilla Firefox/firefox.exe'
-    // browser: 'microsoft-edge:http://localhost:3000'
-  });
+  exports.build = series(
+    copy,
+    css,
+    js,
+    index,
+  );
 
-  gulp.watch('./src/assets/scss/styles.scss', ['scss']);
-  gulp.watch('./src/assets/js/main.js', ['js']);
-  gulp.watch(['./template/**/*.html', './src/**/*.yml', './src/**/*.md'], ['index']);
-  gulp.watch(['./src/assets/img/*', './src/assets/pdf/*'], ['copy']).on('change', browserSync.reload);
-});
-
-gulp.task('build', (done) => {
-  sequence('clean', ['js', 'scss', 'copy', 'index'])(done);
-});
+  exports.default = parallel(
+    copy,
+    css,
+    js,
+    index,
+    browsersyncServe,
+    watchTask
+  );
